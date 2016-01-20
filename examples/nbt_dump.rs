b2ebf7ff8cafbb1f094aba26342489b50a413e43
Andrew Chin <achin@eminence32.net>
2016-01-19 22:47:46 -0500

    Add an nbt_dump example app

---
+++ examples/nbt_dump.rs
@@ -0,0 +1,24 @@
+extern crate nbtrs;
+extern crate flate2;
+
+use nbtrs::Tag;
+
+use std::env::args;
+use std::fs::File;
+use flate2::read::GzDecoder;
+
+fn load_and_print(s: &str) {
+    println!("{}", s);
+    let f = File::open(s).unwrap();
+    let mut decoder = GzDecoder::new(f).unwrap();
+    let (name, tag) = Tag::parse(&mut decoder).unwrap();
+    tag.pretty_print(0, Some(&name));
+}
+
+fn main() {
+
+    for arg in args().skip(1) {
+        load_and_print(&arg);
+    }
+
+}
