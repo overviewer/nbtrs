8657799275320c8df595185b15e8dc62129ec99c
Andrew Chin <achin@eminence32.net>
2016-01-23 00:11:08 -0500

    Fixup for nbt_dump example

--- examples/nbt_dump.rs
+++ examples/nbt_dump.rs
@@ -1,18 +1,20 @@
 extern crate nbtrs;
 extern crate flate2;
 
-use nbtrs::Tag;
+use nbtrs::{Tag, Taglike};
 
 use std::env::args;
 use std::fs::File;
 use flate2::read::GzDecoder;
 
 fn load_and_print(s: &str) {
-    println!("{}", s);
+    println!("Dumping... {}", s);
     let f = File::open(s).unwrap();
     let mut decoder = GzDecoder::new(f).unwrap();
     let (name, tag) = Tag::parse(&mut decoder).unwrap();
-    tag.pretty_print(0, Some(&name));
+
+    let pos = tag.key("Data").key("Player").key("Pos").as_list().unwrap().clone();
+    tag.key("Data").key("Player").key("Attributes").unwrap().pretty_print(0, Some("Player"));
 }
 
 fn main() {
